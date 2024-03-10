acfb14f7edb772805197bd9029814100fd92cf0a
dannykay62 <danielkay62@gmail.com>
2024-03-10 11:17:59 +0100

    Created the HTML layout for the index page and display news

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import News, Category
 
-# Create your views here.
+
+def index(request):
+    first_news = News.objects.first()
+    four_news = News.objects.all()[1:5]
+    three_categories = Category.objects.all()[0:3]
+
+    return render(request, 'index.html', {
+        'first_news': first_news,
+        'four_news': four_news,
+        'three_categories': three_categories
+    })
